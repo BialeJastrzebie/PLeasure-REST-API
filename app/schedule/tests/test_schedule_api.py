74bcf5f65f9658ab91cab2eee2c41a5b02657e03
Krystian Juszczyk <juszczyk-krystian@wp.pl>
2024-04-29 23:23:48 +0200

    Removed unused import

--- app/schedule/tests/test_schedule_api.py
+++ app/schedule/tests/test_schedule_api.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Lesson, Schedule
+from core.models import Schedule
 
 from schedule.serializers import ScheduleSerializer
 
