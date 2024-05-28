6983432dd342f7867731fdc0ed6a8495b934e8d9
Krystian Juszczyk <juszczyk-krystian@wp.pl>
2024-05-28 16:45:06 +0200

    Fixed linting.

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,4 @@
 from rest_framework import viewsets, mixins
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 
 
 class BaseViewSet(viewsets.GenericViewSet,
